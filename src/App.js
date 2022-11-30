ccf7bdf4c791b6171fe70f30f5ae2908ee87f87a
Enikő Valkó <eaaenva@students.eaaa.dk>
2022-11-30 11:56:14 +0100

    jaj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
+import Nav from "./components/nav"
+import PreplistsOrderlists from "./pages/PreplistsOrderlists"
+import Allergies from "./pages/Allergies"
+import ClosingList from "./pages/ClosingList"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <Nav />
+    <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/list" element={<PreplistsOrderlists />} />
+        <Route path="/allergies" element={<Allergies />} />
+        <Route path="/closing" element={<ClosingList />} />
+        <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+</main>
   );
 }
 
