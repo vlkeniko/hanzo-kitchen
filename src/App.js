3ea4ca44514bca1c180292b21b3f163aa5fdeb15
Enikő Valkó <eaaenva@students.eaaa.dk>
2022-11-30 15:21:05 +0100

    jaj

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
-import Nav from "./components/nav"
+import Nav from "./components/Nav"
 import PreplistsOrderlists from "./pages/PreplistsOrderlists"
 import Allergies from "./pages/Allergies"
 import ClosingList from "./pages/ClosingList"
--- src/pages/Allergies.js
+++ src/pages/Allergies.js
@@ -1,9 +1,10 @@
-
 import React from 'react'
+import Nav from "../components/Nav"
 
 export default function Allergies() {
   return (
     <div>
+      <Nav/>
       
     </div>
   )
