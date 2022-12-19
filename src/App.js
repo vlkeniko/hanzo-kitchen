3df1894c75e823246b6ab232fa05842ba4cfa9b6
Enikő Valkó <eaaenva@students.eaaa.dk>
2022-12-19 10:58:27 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -7,43 +7,41 @@ import ListMakerIngredients from './pages/ListMakerIngredients';
 import Overview from './pages/Overview';
 import ListMakerDishesM from './pages/ListMakerDishesM';
 import PreplistsOrderlists from './pages/PreplistsOrderlists';
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Preplist from "./pages/Preplist"
 
 
 function App() {
 
-
-
   return (
     <main>
-    <Routes>
-      <Route path="/dishes" element={<ListMakerDishesM/>} />
-      <Route path="/ingredients" element={<ListMakerIngredients/>} />
-      <Route path="/construction" element={<Construction/>} />
-      <Route path="/lists" element={<PreplistsOrderlists/>} />
-      <Route path="/preplist" element={<Preplist/>} />
-      <Route path="/overview" element={<Overview/>} />
-      <Route path="/" element={<HomePage/>} />
- 
-    </Routes>
-    
-            <ToastContainer 
-position="top-center"
-autoClose={300}
-limit={0}
-hideProgressBar={true}
-newestOnTop={true}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"/>
+      <Routes>
+        <Route path="/dishes" element={<ListMakerDishesM />} />
+        <Route path="/ingredients" element={<ListMakerIngredients />} />
+        <Route path="/construction" element={<Construction />} />
+        <Route path="/lists" element={<PreplistsOrderlists />} />
+        <Route path="/preplist" element={<Preplist />} />
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/" element={<HomePage />} />
+
+      </Routes>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={300}
+        limit={0}
+        hideProgressBar={true}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light" />
 
     </main>
-   
+
   );
 }
 
